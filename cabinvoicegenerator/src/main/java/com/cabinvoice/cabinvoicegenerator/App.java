787ae3bcc3d3eea3911e9a7991661cda9da1d813
bhaskar611 <bhaskarnavuluri9@gmail.com>
2021-03-15 12:43:57 +0530

    Added

---
+++ cabinvoicegenerator/src/main/java/com/cabinvoice/cabinvoicegenerator/App.java
@@ -0,0 +1,13 @@
+package com.cabinvoice.cabinvoicegenerator;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
