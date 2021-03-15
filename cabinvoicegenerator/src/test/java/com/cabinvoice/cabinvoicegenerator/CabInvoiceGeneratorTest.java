25eadc1a7a84ea14e74cfb8c798c4d59d883a83c
bhaskar611 <bhaskarnavuluri9@gmail.com>
2021-03-15 13:54:14 +0530

    Added:caliculate fare

---
+++ cabinvoicegenerator/src/test/java/com/cabinvoice/cabinvoicegenerator/CabInvoiceGeneratorTest.java
@@ -0,0 +1,27 @@
+package com.cabinvoice.cabinvoicegenerator;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class CabInvoiceGeneratorTest 
+{
+    /**
+     * Tests the whether total fare generation is correct.
+     */
+    @Test
+    public void invoiceGeneratorTest_shouldReturnTotalFare(){
+        assertEquals(new Double(15.0), InvoiceGenerator.totalFare(1,5));
+    }
+    /**
+     * Tests whether total fare returned is minimal
+     */
+    @SuppressWarnings("deprecation")
+	@Test
+    public void invoiceGeneratorTest_shouldReturnMinimalFare(){
+        assertEquals(new Double(5.0), InvoiceGenerator.totalFare(0.2,2));
+    }
+}
